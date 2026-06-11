3df53c9f7a079b3f536eaa5ac434033660b0217b
Agent! for MacOS <todd@starplayrx.com>
2026-06-10 21:33:07 -0400

    rock lines 2px thick

--- AsteroidZ/GameScene.swift
+++ AsteroidZ/GameScene.swift
@@ -590,7 +590,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             line.addLine(to: CGPoint(x: b.x - (b.x - a.x) * d, y: b.y - (b.y - a.y) * d))
             let seg = SKShapeNode(path: line)
             seg.strokeColor = SKColor(white: 1, alpha: 0.5)
-            seg.lineWidth = 1.0
+            seg.lineWidth = 2.0
             seg.blendMode = .add
             node.addChild(seg)
         }
