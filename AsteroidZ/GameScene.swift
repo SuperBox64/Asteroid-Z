93e6f0325a10bfaf5fdcf6ebcbca8f3b8fa69f50
Agent! for MacOS <todd@starplayrx.com>
2026-06-10 21:16:19 -0400

    rock lines another pixel longer at each end

--- AsteroidZ/GameScene.swift
+++ AsteroidZ/GameScene.swift
@@ -577,7 +577,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             let a = pts[i]
             let b = pts[(i + 1) % pts.count]
             let len = hypot(b.x - a.x, b.y - a.y)
-            let d = max(0, len * 0.07 - 1) / max(len, 0.001)
+            let d = max(0, len * 0.07 - 2) / max(len, 0.001)
             let line = CGMutablePath()
             line.move(to: CGPoint(x: a.x + (b.x - a.x) * d, y: a.y + (b.y - a.y) * d))
             line.addLine(to: CGPoint(x: b.x - (b.x - a.x) * d, y: b.y - (b.y - a.y) * d))
